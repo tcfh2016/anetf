03bd71375814adfd135f5e6123791bb5a4cc71ce
tcfh2016 <tcfh2016@gmail.com>
2024-04-07 21:58:25 +0800

    udpate

--- src/csindex.py
+++ src/csindex.py
@@ -1,5 +1,15 @@
-from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
 
-driver = webdriver.Firefox()
-#driver.get("http://www.python.org")
-#assert "Python" in driver.title
+target_url = 'https://www.csindex.com.cn/#/indices/family/list'
+
+def scrap_page(url):
+    response = requests.get(url, verify=False)
+
+    if response.status_code != 200:
+        return f'status failed with {response.status_code}'
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        print(soup)
+
+scrap_page(target_url)
